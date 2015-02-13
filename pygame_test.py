dee3ce864c1716eb1bcd71ff5f000664cebf776a
Dominic Hamon <dma+github@stripysock.com>
2015-02-13 10:26:50 -0800

    cleanup

--- pygame_test.py
+++ pygame_test.py
@@ -2,37 +2,47 @@ import sys, pygame
 
 pygame.init()
 
-size = width, height = 320, 200
-speed = [2,0]
-black = (0,0,0)
+SIZE = WIDTH, height = 320, 200
+BLACK = (0,0,0)
 
 TIMEREVENT = pygame.USEREVENT
 
 FPS = 60
 
+class Ball:
+  def __init__(self, image, x, y, speed):
+    self._speed = speed
+    self.image = image
+    self.pos = image.get_rect().move(x, y)
+
+  def move(self):
+    self.pos = self.pos.move(self._speed)
+    if self.pos.left < 0 or self.pos.right > WIDTH:
+      self._speed[0] = self._speed[0] * -0.9
+    if self.pos.top < 0 or self.pos.bottom > height:
+      self._speed[1] = self._speed[1] * -0.9
+
+    self._speed[1] += 0.1
+
 def main():
   pygame.init()
   pygame.time.set_timer(TIMEREVENT, 1000 / FPS)
 
-  screen = pygame.display.set_mode(size)
+  screen = pygame.display.set_mode(SIZE)
 
-  ball = pygame.image.load("ball.gif")
-  ballrect = ball.get_rect()
+  image = pygame.image.load("ball.gif")
+
+  ball = Ball(image, 0, 0, [2, 0])
 
   while True:
     ev = pygame.event.wait()
 
     if ev.type == TIMEREVENT:
-      ballrect = ballrect.move(speed)
-      if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -0.9 * speed[0]
-      if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -0.9 * speed[1]
+      ball.move()
 
-      speed[1] = speed[1] + 0.1
+      screen.fill(BLACK)
+      screen.blit(ball.image, ball.pos)
 
-      screen.fill(black)
-      screen.blit(ball, ballrect)
       pygame.display.flip()
     elif ev.type == pygame.KEYDOWN and ev.key == pygame.K_ESCAPE:
       break
